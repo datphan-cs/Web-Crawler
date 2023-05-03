25e46f980db7e3b6151a302e1eddfdaa462810d2
chill-cats <public.hoangnhanlenguyen@gmail.com>
2023-05-03 17:38:20 +0700

    Add css

--- src/feature.php
+++ src/feature.php
@@ -1,27 +1,24 @@
 <?php
 
-function prototype ($option, &$file_type, &$tag_name, &$attr){
-	if ($option == 'img'){
+function prototype($option, &$file_type, &$tag_name, &$attr)
+{
+	if ($option == 'img') {
 		$file_type = ['.jpg', '.png', '.gif'];
 		$tag_name = 'img';
 		$attr = 'src';
-	}
-	else if ($option == 'doc'){
+	} else if ($option == 'doc') {
 		$file_type = ['.pdf', '.doc', '.docx'];
 		$tag_name = 'a';
 		$attr = 'href';
-	}
-	else if ($option == 'csv'){
+	} else if ($option == 'csv') {
 		$file_type = ['.csv', '.xlsx'];
 		$tag_name = 'a';
 		$attr = 'href';
-	}
-	else if ($option == 'zip'){
+	} else if ($option == 'zip') {
 		$file_type = ['.tar.gz', '.zip', '.rar', '.7z'];
 		$tag_name = 'a';
 		$attr = 'href';
-	}
-	else if ($option == 'sound'){
+	} else if ($option == 'sound') {
 		$file_type = ['.mp3', '.wav'];
 		$tag_name = 'a';
 		$attr = 'href';
@@ -29,7 +26,8 @@ function prototype ($option, &$file_type, &$tag_name, &$attr){
 }
 
 // Finish books_toscrape: get data from all page
-function crawl_toscrape(&$url, $option, &$link_array){
+function crawl_toscrape(&$url, $option, &$link_array)
+{
 	$http_client = new \GuzzleHttp\Client();
 
 	$baseurl = "http://books.toscrape.com/";
@@ -40,46 +38,47 @@ function crawl_toscrape(&$url, $option, &$link_array){
 
 	prototype($option, $file_type, $tag_name, $place);
 	$page = 1;
-	while (True){
+	while (True) {
 		// echo "Page number: $page <br>";
 		$response = $http_client->get($url);
-		
+
 		$html_string = (string) $response->getBody();
 		//add this line to suppress any warnings
 		libxml_use_internal_errors(true);
-		
+
 		$doc = new DOMDocument();
 		$doc->loadHTML($html_string);
-		
+
 		$file_crawled = new DOMXPath($doc);
 
-		foreach($file_crawled->query('//ol[@class="row"]//li//article//div//a/img') as $image) {
+		foreach ($file_crawled->query('//ol[@class="row"]//li//article//div//a/img') as $image) {
 			$data = $image->getAttribute($place);
-			if (strpos($data,'//') === False){
+			if (strpos($data, '//') === False) {
 				$data = $baseurl . $data;
 			}
-			if (strpos($data,"../") !== False){
-				$data = str_replace("../","",$data);
+			if (strpos($data, "../") !== False) {
+				$data = str_replace("../", "", $data);
 			}
-			array_push($link_array,$data);
+			array_push($link_array, $data);
 
 		}
 		// [ ]Cannot identified if next_page exist or not
 		$next_page = $file_crawled->query('//div//ul//li[@class="next"]/a');
-		if ($next_page->length > 0){
+		if ($next_page->length > 0) {
 			$next_data = $next_page[0]->getAttribute('href');
-			if (strpos($next_data, 'catalogue/') === False){
+			if (strpos($next_data, 'catalogue/') === False) {
 				$next_data = 'catalogue/' . $next_data;
 			}
 			$url = $baseurl . $next_data;
 			$page += 1;
 		}
-		if (count($link_array) >=50){
+		if (count($link_array) >= 50) {
 			break;
 		}
 	}
 }
-function crawl_pdfdrive(&$url, $option, &$link_array){
+function crawl_pdfdrive(&$url, $option, &$link_array)
+{
 	// $http_client = new \GuzzleHttp\Client();
 
 	// $baseurl = "https://www.pdfdrive.com";
@@ -93,14 +92,14 @@ function crawl_pdfdrive(&$url, $option, &$link_array){
 	// $page = 1;
 	// 	// echo "Page number: $page <br>";
 	// 	$response = $http_client->get($url);
-		
+
 	// 	$html_string = (string) $response->getBody();
 	// 	//add this line to suppress any warnings
 	// 	libxml_use_internal_errors(true);
-		
+
 	// 	$doc = new DOMDocument();
 	// 	$doc->loadHTML($html_string);
-		
+
 	// 	$file_crawled = new DOMXPath($doc);
 
 	// 	foreach($file_crawled->query('//div[@class="file-right"]/a') as $mid_page) {
@@ -125,10 +124,10 @@ function crawl_pdfdrive(&$url, $option, &$link_array){
 	// 	// 	$html_string = (string) $response->getBody();
 	// 	// 	//add this line to suppress any warnings
 	// 	// 	libxml_use_internal_errors(true);
-			
+
 	// 	// 	$doc = new DOMDocument();
 	// 	// 	$doc->loadHTML($html_string);
-			
+
 	// 	// 	$file_crawled = new DOMXPath($doc);
 
 	// 	// }
@@ -140,24 +139,25 @@ function crawl_pdfdrive(&$url, $option, &$link_array){
 	// 	 * @see JonnyW\PhantomJs\Http\Request
 	// 	 **/
 	// 	$request = $client->getMessageFactory()->createRequest($mid_page_array[0], 'GET');
-	
+
 	// 	/** 
 	// 	 * @see JonnyW\PhantomJs\Http\Response 
 	// 	 **/
 	// 	$response = $client->getMessageFactory()->createResponse();
-	
+
 	// 	// Send the request
 	// 	$client->send($request, $response);
-	
+
 	// 	if($response->getStatus() === 200) {
-	
+
 	// 		// Dump the requested page content
 	// 		echo $response->getContent();
 	// 	}
-	
+
 }
 
-function crawl_catalog(&$url, $option, &$link_array){
+function crawl_catalog(&$url, $option, &$link_array)
+{
 	$http_client = new \GuzzleHttp\Client();
 
 	$baseurl = "https://catalog.data.gov";
@@ -168,34 +168,33 @@ function crawl_catalog(&$url, $option, &$link_array){
 
 	prototype($option, $file_type, $tag_name, $place);
 	$page = 1;
-	while (count($link_array) <=10 and $page <=10) {
+	while (count($link_array) <= 10 and $page <= 10) {
 		// echo "Page number: $page <br>";
 		$response = $http_client->get($url);
-		
+
 		$html_string = (string) $response->getBody();
 		//add this line to suppress any warnings
 		libxml_use_internal_errors(true);
-		
+
 		$doc = new DOMDocument();
 		$doc->loadHTML($html_string);
-		
+
 		$file_crawled = new DOMXPath($doc);
 
-		foreach($file_crawled->query('//ul[@class="dataset-resources unstyled"]//li/a') as $link) {
+		foreach ($file_crawled->query('//ul[@class="dataset-resources unstyled"]//li/a') as $link) {
 			$data = $link->getAttribute($place);
-			foreach($file_type as $extension){
-				if (strpos($data, $extension) !== False){
-					array_push($link_array,$data);
+			foreach ($file_type as $extension) {
+				if (strpos($data, $extension) !== False) {
+					array_push($link_array, $data);
 				}
 			}
 		}
 		$next_page = $file_crawled->query('//div//ul[@class="pagination"]//li/a');
-		if ($next_page->length > 0){
+		if ($next_page->length > 0) {
 			$number_of_page = $next_page->length;
-			$next_data = $next_page[$number_of_page-1]->getAttribute('href');
+			$next_data = $next_page[$number_of_page - 1]->getAttribute('href');
 			$url = $baseurl . $next_data;
 			$page += 1;
 		}
 	}
 }
-?>
--- src/index.php
+++ src/index.php
@@ -1,121 +1,135 @@
-<?php 
-	
-	///////////////// Array to store all links to data /////////////////
-	$extracted_link = [];
-	///////////////// Crawl data from specified URL /////////////////
-	if (isset($_POST['url'])){
-		if (isset($_POST['down_all'])){
-			$extracted_link = unserialize($_POST['data']);
+<?php
 
-			$zip = new ZipArchive();
+///////////////// Array to store all links to data /////////////////
+$extracted_link = [];
+///////////////// Crawl data from specified URL /////////////////
+if (isset($_POST['url'])) {
+	if (isset($_POST['down_all'])) {
+		$extracted_link = unserialize($_POST['data']);
 
-			# create a temp file & open it
-			$tmp_file = tempnam('.', '');
-			$zip->open($tmp_file, ZipArchive::CREATE);
+		$zip = new ZipArchive();
 
-			$i = 0;
-			foreach ($extracted_link as $file) {
-				# download file
-				if ($i >=10){
-					break;
-				}
-				$i++;
-				$download_file = file_get_contents($file);
-				$zip->addFromString(basename($file), $download_file);
+		# create a temp file & open it
+		$tmp_file = tempnam('.', '');
+		$zip->open($tmp_file, ZipArchive::CREATE);
+
+		$i = 0;
+		foreach ($extracted_link as $file) {
+			# download file
+			if ($i >= 10) {
+				break;
 			}
-
-			# close zip
-			$zip->close();
-
-			# send the file to the browser as a download
-			header('Content-disposition: attachment; filename="my file.zip"');
-			header('Content-type: application/zip');
-			readfile($tmp_file);
-			unlink($tmp_file);
-			// foreach ($extracted_link as $link){
-			// 	$file_name = basename($link);
-			// 	$file_path = "download/";
-			// 	$file_name = $file_path . $file_name;
-			// 	if (file_put_contents($file_name, file_get_contents($link))){
-			// 		echo "File downloaded successfully";
-			// 	}
-			// 	else{
-			// 		echo "File downloading failed.";
-			// 	}
-			// }
+			$i++;
+			$download_file = file_get_contents($file);
+			$zip->addFromString(basename($file), $download_file);
 		}
-		else{
-			require 'vendor/autoload.php';
-			require 'feature.php';
 
-			$url = $_POST['url'];
-			$option = $_POST['radio'];
-			if (strpos($url,"books.toscrape.com") !== False){
-				crawl_toscrape($url, $option, $extracted_link);			
-			}
-			else if (strpos($url,"pdfdrive") !== False){
-				crawl_pdfdrive($url, $option, $extracted_link);
-			}
-			else if (strpos($url, "catalog.data.gov") !== False){
-				crawl_catalog($url, $option, $extracted_link);
-			}
+		# close zip
+		$zip->close();
 
-
-		
-	}
-	}
-?>
-<form action="<?php echo $_SERVER['PHP_SELF'] ?>" name="form" method="POST">
-  <input type="text" name="url" id="url" placeholder = "Enter the link you want to scrape here:">
-  <br>
-  <br>
-  <fieldset>
-    <div>
-      <input type="radio" id="img" name="radio" value = "img" checked>
-      <label for="img">Image File (.png, .jpg, .gif) </label>
-    </div>
-
-    <div>
-      <input type="radio" id="doc" name="radio" value = "doc">
-      <label for="pdf">Document (.pdf, .doc, .docx) </label>
-    </div>
-
-    <div>
-      <input type="radio" id="csv" name="radio" value = "csv">
-      <label for="csv">Worksheet (.csv, .xlsx) </label>
-    </div>
-
-    <div>
-      <input type="radio" id="zip" name="radio" value = "zip">
-      <label for="zip">Compressed File (.tar.gz, .zip, .rar, .7z) </label>
-    </div>
-    <div>
-      <input type="radio" id="sound" name="radio" value = "sound">
-      <label for="sound">Music File (.mp3, .wav) </label>
-    </div>
-
-	<br>
-	</fieldset>
-  <br/>
-  <input type="submit" name="my_form_submit_button" value="CRAWL"/>
-  <input type='hidden' name='data' value="<?php echo htmlentities(serialize($extracted_link)); ?>" />
-  <input type="submit" id ="down_btn" name="down_all" value="DOWNLOAD ALL"/>
-</form>
-
-<?php	
-
-	if (empty($extracted_link) === True && isset($_POST['url'])){
-		echo "THIS WEBSITE MAY NOT HAVE YOUR DESIRED FILE TYPE";
-	}
-	else{
-		// for ($i = 20; $i < 25; $i++){
-		// 	echo "<a href = $extracted_link[$i]>$extracted_link[$i] </a> <br>";
+		# send the file to the browser as a download
+		header('Content-disposition: attachment; filename="my file.zip"');
+		header('Content-type: application/zip');
+		readfile($tmp_file);
+		unlink($tmp_file);
+		// foreach ($extracted_link as $link){
+		// 	$file_name = basename($link);
+		// 	$file_path = "download/";
+		// 	$file_name = $file_path . $file_name;
+		// 	if (file_put_contents($file_name, file_get_contents($link))){
+		// 		echo "File downloaded successfully";
+		// 	}
+		// 	else{
+		// 		echo "File downloading failed.";
+		// 	}
 		// }
-		foreach ($extracted_link as $link){
-			echo "<a href = $link>$link </a> <br>";
+	} else {
+		require '../vendor/autoload.php';
+		require 'feature.php';
+
+		$url = $_POST['url'];
+		$option = $_POST['radio'];
+		if (strpos($url, "books.toscrape.com") !== False) {
+			crawl_toscrape($url, $option, $extracted_link);
+		} else if (strpos($url, "pdfdrive") !== False) {
+			crawl_pdfdrive($url, $option, $extracted_link);
+		} else if (strpos($url, "catalog.data.gov") !== False) {
+			crawl_catalog($url, $option, $extracted_link);
 		}
+
+
+
 	}
-
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+		crossorigin="anonymous"></script>
+	<title>Document</title>
+</head>
+
+<body class="container-fluid">
+	<h1>Simple web crawler tool</h1>
+	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" name="form" method="POST" class="mb-4">
+		<div class="mb-3">
+			<label class="form-label" for="url">Enter the link you want to crawl for:</label>
+			<input type="text" name="url" id="url" class="form-control" placeholder="https://...">
+		</div>
+		<div class="mb-3">
+			<legend for="select-filetype">File type:</legend>
+			<fieldset id="select-filetype">
+				<input type="radio" id="img" name="radio" value="img" class="form-check-input me-1" checked>
+				<label for="img" class="form-check-label">Image File (.png, .jpg, .gif)</label>
+
+				<div>
+					<input type="radio" id="doc" name="radio" value="doc" class="form-check-input me-1">
+					<label for="doc" class="form-check-label">Document (.pdf, .doc, .docx) </label>
+				</div>
+
+				<div>
+					<input type="radio" id="csv" name="radio" value="csv" class="form-check-input me-1">
+					<label for="csv" class="form-check-label">Worksheet (.csv, .xlsx) </label>
+				</div>
+
+				<div>
+					<input type="radio" id="zip" name="radio" value="zip" class="form-check-input me-1">
+					<label for="zip" class="form-check-label">Compressed File (.tar.gz, .zip, .rar, .7z) </label>
+				</div>
+				<div>
+					<input type="radio" id="sound" name="radio" value="sound" class="form-check-input me-1">
+					<label for="sound" class="form-check-label">Music File (.mp3, .wav) </label>
+				</div>
+
+				<br>
+			</fieldset>
+		</div>
+		<div class="col">
+			<input type="submit" name="my_form_submit_button" value="CRAWL" class="btn btn-primary" />
+			<input type='hidden' name='data' value="<?php echo htmlentities(serialize($extracted_link)); ?>" />
+			<input type="submit" id="down_btn" name="down_all" value="DOWNLOAD ALL" class="btn btn-secondary" <?php echo empty($extracted_link) === True && isset($_POST['ur']) ? "" : "disabled" ?> />
+		</div>
+	</form>
+	<h2>Result</h2>
+	<?php if (empty($extracted_link) === True && isset($_POST['url'])) { ?>
+		<p>"THIS WEBSITE MAY NOT HAVE YOUR DESIRED FILE TYPE";</p>
+	<?php } else { ?>
+		<?php foreach ($extracted_link as $link) { ?>
+			<div class="border mb-1 p-3 rounded">
+				URL: <a href=$link><?= $link ?></a>
+			</div>
+		<?php } ?>
+	<?php } ?>
 
 
+</body>
+
+</html>
